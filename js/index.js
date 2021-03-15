7b058f307280b36f000a99a4377e1e2e139bbaf6
Zhixiang <zhixiangteoh@gmail.com>
2021-03-15 02:32:38 -0400

    CSS Globals, some UI changes
    Changes:
    - Add new Dark/Light mode button (Note: probably not responsive)
    - Use CSS variables and `html[data-theme]` to handle light/dark mode
    - Added `logo.svg` for vectorized logo in navbar
    - Removed `Home` in navbar
    - Added new DSC UMich logo
    
    Let me know if anything looks off!

--- js/index.js
+++ js/index.js
@@ -1,23 +1,3 @@
-function changeButtonHTML(obj) {
-  var element = document.body;
-  var head = document.getElementById("headerContainer");
-  var headText = document.getElementById("header");
-  if (obj.innerHTML == "Dark Mode") {
-    obj.innerHTML = "Light Mode";
-    element.classList.toggle("dark2");
-    head.classList.toggle("darkTest");
-    headText.classList.toggle("dark");
-    return;
-  }
-  if (obj.innerHTML == "Light Mode") {
-    obj.innerHTML = "Dark Mode";
-    element.classList.toggle("dark2");
-    head.classList.toggle("dark");
-    headText.classList.toggle("dark");
-    return;
-  }
-}
-
 function goToComPage() {
   window.open("https://dsc.community.dev/university-of-michigan/");
 }
